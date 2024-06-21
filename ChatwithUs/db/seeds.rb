0056770016b341d5536b8cef29d7322cc6f70fcc
Puja Sharma <pujashrma151195@gmail.com>
2024-06-21 17:46:21 +0530

    Changes updated

--- ChatwithUs/db/seeds.rb
+++ ChatwithUs/db/seeds.rb
@@ -7,3 +7,16 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+ User.create(username: "Puja", password: "Password123")
+ User.create(username: "Anushka", password: "Password123")
+ User.create(username: "Shikha", password: "Password123")
+ User.create(username: "Sneha", password: "Password123")
+ User.create(username: "Meenakshi", password: "Password123")
+ User.create(username: "Kritika", password: "Password123")
+
+ Message.create(body: "Hello everyone!!!", user_id: 1)
+ Message.create(body: "Hello Puja, how are you?!!!", user_id: 4)
+ Message.create(body: "I'm fine... sneha", user_id: 1)
+ Message.create(body: "Today's weather is so awesome. Let's go somewhere..", user_id: 3)
+ Message.create(body: "Hello Guys..!!", user_id: 6)
+ Message.create(body: "Feeling good!!", user_id: 5)
