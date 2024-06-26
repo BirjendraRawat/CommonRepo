ac5c6216c9419d0ac6b343d03214bcfcb2962022
Puja Sharma <pujashrma151195@gmail.com>
2024-06-26 15:43:23 +0530

    Admin crud actions created

--- e_commerce/config/routes.rb
+++ e_commerce/config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'admins/index'
+  get 'admins/new'
+  get 'admins/create'
+  get 'admins/edit'
+  get 'admins/update'
+  get 'admins/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
