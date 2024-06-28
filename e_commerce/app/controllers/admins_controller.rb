6d79fcf25c1f92d593dc2ced2c1aa33356c0ae8e
Puja Sharma <pujashrma151195@gmail.com>
2024-06-28 15:03:23 +0530

    Curd of admin updated

--- e_commerce/app/controllers/admins_controller.rb
+++ e_commerce/app/controllers/admins_controller.rb
@@ -1,19 +1,53 @@
 class AdminsController < ApplicationController
+before_action :set_admin, only: [:edit, :update, :destroy]
+
   def index
+    @admins = Admin.all
   end
 
   def new
+    @admin = Admin.new
   end
 
   def create
+    @admin = Admin.new(admin_params)
+      if @admin.save
+      flash[:success] = "Welcome to the Admin Page."
+      redirect_to admins_url
+      else
+      render 'new'
+    end
   end
 
   def edit
   end
 
   def update
+    if @admin.update(admin_params)
+      flash[:success] = "Your account was updated successfully"
+      redirect_to admins_path
+      render 'edit'
+    end
+  end
+
+  def show
   end
 
   def destroy
+    @admin.destroy
+    flash[:danger] = "Admin and all products created by admin have been deleted"
+     redirect_to admins_path
   end
+
+  private
+
+  def admin_params
+    params.require(:admin).permit(:name, :email, :password_digest, :address, :address2)
+  end
+
+  def set_admin
+    @admin = Admin.find(params[:id])
+  end
+
+
 end
--- e_commerce/config/routes.rb
+++ e_commerce/config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  get 'admins/index'
-  get 'admins/new'
-  get 'admins/create'
-  get 'admins/edit'
-  get 'admins/update'
-  get 'admins/destroy'
+
+  get 'signup', to: 'admins#new'
+  get 'admins', to: 'admins#create'
+  resources :admins
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
